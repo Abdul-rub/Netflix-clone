1039d4a6cf9728dec3f282d7868bee7f1e9fd0bb
Abdul Rub <arub0419@gmail.com>
2022-10-03 14:21:22 +0530

    basic-setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDpz-rr9yD0QSmvgWWTJrfGxEGoUbrXRi0",
+    authDomain: "netflix-clone-41226.firebaseapp.com",
+    projectId: "netflix-clone-41226",
+    storageBucket: "netflix-clone-41226.appspot.com",
+    messagingSenderId: "445691577717",
+    appId: "1:445691577717:web:d13f7c494a0835a0467f3c"
+  };
